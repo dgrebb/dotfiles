b99ee98b00973c338e87a8d009cba35c5fbf73dc
Dan Grebb <git@dgrebb.com>
2024-04-30 04:15:07 -0400

    feat(sketchybar): set up script for screensaver settings

---
+++ .dotfiles/cron/ss.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+osascript -e 'tell application "System Events" to set current screen saver to screen saver "Drift"' -e 'tell application "System Events" to set delay interval of screen saver preferences to 600 -- Number In Seconds'
