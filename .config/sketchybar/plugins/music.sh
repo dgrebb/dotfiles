720bb7381b93e9ae743c302cc893ef178779c206
Dan Grebb <git@dgrebb.com>
2024-06-21 07:16:04 -0400

    fix(sketchybar): include full artist name in JSON output

--- .config/sketchybar/plugins/music.sh
+++ .config/sketchybar/plugins/music.sh
@@ -26,7 +26,7 @@ if [[ $PLAYER_STATE == "stopped" ]]; then
 fi
 
 TITLE=$(osascript -e 'tell application "Music" to get name of current track')
-ARTIST=$(osascript -e 'tell application "Music" to get artist of current track')
+OGARTIST=$(osascript -e 'tell application "Music" to get artist of current track')
 ALBUM=$(osascript -e 'tell application "Music" to get album of current track')
 if [[ "$MACHINE" == 'office' ]]; then
   LOVED=$(osascript -l JavaScript -e "Application('Music').currentTrack().loved()")
@@ -53,8 +53,8 @@ if [[ ${#TITLE} -gt 25 ]]; then
   TITLE=$(printf "$(echo $TITLE)")
 fi
 
-if [[ ${#ARTIST} -gt 25 ]]; then
-  ARTIST=$(printf "$(echo $ARTIST | cut -c 1-25)…")
+if [[ ${#OGARTIST} -gt 25 ]]; then
+  ARTIST=$(printf "$(echo $OGARTIST | cut -c 1-25)…")
 fi
 
 if [[ ${#ALBUM} -gt 25 ]]; then
@@ -81,6 +81,6 @@ sketchybar -m --set music.album \
   label="« ${ALBUM}" \
   drawing=$DRAWING
 
-echo "{\"artist\": \"${ARTIST}\", \"album\": \"${ALBUM}\", \"title\": \"${TITLE}\", \"loved\": ${LOVED}}" >$dboard_music_json
-echo "{\"artist\": \"${ARTIST}\", \"album\": \"${ALBUM}\", \"title\": \"${TITLE}\", \"loved\": ${LOVED}}" >$dev_dboard_music_json
-echo "{\"artist\": \"${ARTIST}\", \"album\": \"${ALBUM}\", \"title\": \"${TITLE}\", \"loved\": ${LOVED}}" >$prev_dboard_music_json
+echo "{\"artist\": \"${OGARTIST}\", \"album\": \"${ALBUM}\", \"title\": \"${TITLE}\", \"loved\": ${LOVED}}" >$dboard_music_json
+echo "{\"artist\": \"${OGARTIST}\", \"album\": \"${ALBUM}\", \"title\": \"${TITLE}\", \"loved\": ${LOVED}}" >$dev_dboard_music_json
+echo "{\"artist\": \"${OGARTIST}\", \"album\": \"${ALBUM}\", \"title\": \"${TITLE}\", \"loved\": ${LOVED}}" >$prev_dboard_music_json
