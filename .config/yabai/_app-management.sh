e8c7a6ef1c272973eb2fe8a44fb508f1aeaee1cd
Dan Grebb <git@dgrebb.com>
2024-06-23 15:40:07 -0400

    refactor(yabai): remove legacy `window_animation_frame_rate` configuration; apply all rules at end of `yabairc`

--- .config/yabai/_app-management.sh
+++ .config/yabai/_app-management.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Set always-opaque Apps and Windows
+yabai -m rule --add label="Min" app="^Min$" manage=on opacity=1
+
 # Fix Problematic Windows
 yabai -m rule --add app="^(Digital Colou?r Meter|Classic Color Meter)$" sticky=on
-yabai -m rule --add app="^(Glucose Graph|Verve|zoom.us|Zoom|Contrast|Min|Gifox|Bartender 5|CleanMyMac X|Airfoil|Messages|1Password Helper|1Password Helper (GPU)|Fantastical|Fantastical Helper|com.flexibits.fantastical.core.libical-XPC|Keyboard Maestro|TextExpander|Calculator|Software Update|Dictionary|VLC|System Preferences|System Settings|zoom.us|Photo Booth|Archive Utility|Python|LibreOffice|App Store|Steam|Alfred|Activity Monitor|sketchybar)$" manage=off
+yabai -m rule --add app="^(Glucose Graph|Verve|zoom.us|Zoom|Contrast|Gifox|Bartender 5|CleanMyMac X|Airfoil|Messages|1Password Helper|1Password Helper (GPU)|Fantastical|Fantastical Helper|com.flexibits.fantastical.core.libical-XPC|Keyboard Maestro|TextExpander|Calculator|Software Update|Dictionary|VLC|System Preferences|System Settings|zoom.us|Photo Booth|Archive Utility|Python|LibreOffice|App Store|Steam|Alfred|Activity Monitor|sketchybar)$" manage=off
 yabai -m rule --add label="Safari" app="^Safari$" title="^(General|(Tab|Password|Website|Extension)s|AutoFill|Se(arch|curity)|Privacy|Advance)$" manage=off
 yabai -m rule --add label="Finder" app="^Finder$" title="(Co(py|nnect)|Move|Info|Pref)" manage=off
 yabai -m rule --add label="Safari" app="^Safari$" title="^(General|(Tab|Password|Website|Extension)s|AutoFill|Se(arch|curity)|Privacy|Advance)$" manage=off
 yabai -m rule --add label="About This Mac" app="System Information" title="About This Mac" manage=off
 yabai -m rule --add label="Select file to save to" app="^Inkscape$" title="Select file to save to" manage=off
 yabai -m rule --add label="About This Mac" app="System Information" title="About This Mac" manage=off
-yabai -m rule --add app="^(ProtonVPN|GeekTool|CopyClip 2|xScope|iStat Menus Status|com.bjango.istatmenus.weather|1Password Helper (Renderer)|1Password Browser Helper|^1Password$|Security/Agent)$" manage=off layer=above
+yabai -m rule --add app="^(ProtonVPN|GeekTool|CopyClip 2|xScope|iStat Menus Status|com.bjango.istatmenus.weather|1Password Helper (Renderer)|1Password Browser Helper|^1Password$|Security/Agent)$" manage=off sub-layer=above
--- .config/yabai/_spaces.sh
+++ .config/yabai/_spaces.sh
@@ -130,11 +130,9 @@ else
   yabai -m rule --add app="^Music$" space=1
   yabai -m rule --add app="^(Firefox)$" space=^2
   yabai -m rule --add app="^(Notion|Photoshop|Lightroom|Pym|Slack|Discord|Logic Pro)$" space=3
-  yabai -m rule --add app="^(iTerm2)$" space=^6
+  yabai -m rule --add app="^(iTerm2|Min)$" space=^6
   yabai -m rule --add app="^(OmniFocus|Mail|eM Client|Calendar)$" space=5
   yabai -m rule --add app="^Code$" space=7
   yabai -m rule --add app="^dg project$" space=9
 
 fi
-
-yabai -m rule --apply
