2d670509d710669d27331f356c69fba857cba164
Dan Grebb <git@dgrebb.com>
2024-09-11 01:56:23 -0400

    wip(yabai): remove Finder from unmanaged windows

--- .config/yabai/_app-management.sh
+++ .config/yabai/_app-management.sh
@@ -7,7 +7,7 @@ yabai -m rule --add label="Min" app="^Min$" manage=on opacity=1
 yabai -m rule --add app="^(Digital Colou?r Meter|Classic Color Meter)$" sticky=on
 yabai -m rule --add app="^(Glucose Graph|Verve|zoom.us|Zoom|Contrast|Gifox|Bartender 5|CleanMyMac X|Airfoil|Messages|1Password Helper|1Password Helper (GPU)|Fantastical|Fantastical Helper|com.flexibits.fantastical.core.libical-XPC|Keyboard Maestro|TextExpander|Calculator|Software Update|Dictionary|VLC|System Preferences|System Settings|zoom.us|Photo Booth|Archive Utility|Python|LibreOffice|App Store|Steam|Alfred|Activity Monitor|sketchybar|OmniPlan)$" manage=off
 yabai -m rule --add label="Safari" app="^Safari$" title="^(General|(Tab|Password|Website|Extension)s|AutoFill|Se(arch|curity)|Privacy|Advance)$" manage=off
-yabai -m rule --add label="Finder" app="^Finder$" title="(Co(py|nnect)|Move|Info|Pref)" manage=off
+yabai -m rule --add title="(Co(py|nnect)|Move|Info|Pref)" manage=off
 yabai -m rule --add label="Safari" app="^Safari$" title="^(General|(Tab|Password|Website|Extension)s|AutoFill|Se(arch|curity)|Privacy|Advance)$" manage=off
 yabai -m rule --add label="About This Mac" app="System Information" title="About This Mac" manage=off
 yabai -m rule --add label="Select file to save to" app="^Inkscape$" title="Select file to save to" manage=off
