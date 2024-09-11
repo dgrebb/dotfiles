463aeb2615fdf31d08a34352175c8a586309ab4d
Dan Grebb <git@dgrebb.com>
2024-09-11 08:06:52 -0400

    wip(yabai): add tauri apps to space 6

--- .config/yabai/_spaces.sh
+++ .config/yabai/_spaces.sh
@@ -121,7 +121,7 @@ if [[ "$MACHINE" == 'home' ]]; then
   yabai -m rule --add app="^Music$" space=1
   yabai -m rule --add app="^(Firefox)$" space=^2
   yabai -m rule --add app="^(Notion|Photoshop|Lightroom|Pym|Slack|Discord|Logic Pro)$" space=3
-  yabai -m rule --add app="^(iTerm2|Min)$" space=^6
+  yabai -m rule --add label="^(TauriApp|tauri-app|launchpad|LaunchPad)$" app="^(iTerm2|Min|TauriApp|tauri-app|launchpad|LaunchPad)$" space=^6
   yabai -m rule --add app="^(OmniFocus|Mail|eM Client|Calendar)$" space=5
   yabai -m rule --add app="^Code$" space=^7
   yabai -m rule --add app="^Obsidian$" space=9
