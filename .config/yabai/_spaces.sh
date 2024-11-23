953f0d55b4e1c46ef3e29d8bf3933a28acdfe457
Dan Grebb <git@dgrebb.com>
2024-11-22 23:09:07 -0500

    chore(yabai): set home/controller to space 3

--- .config/yabai/_spaces.sh
+++ .config/yabai/_spaces.sh
@@ -121,8 +121,8 @@ if [[ "$MACHINE" == 'home' ]]; then
   yabai -m rule --add app="^Music$" space=1
   yabai -m rule --add app="^(Firefox|Zen Browser)$" space=^2
   # Add Home/Controller when done setting up
-  # Home|Controller
-  yabai -m rule --add app="^(Notion|Photoshop|Lightroom|Pym|Slack|Discord|Logic Pro|)$" space=3
+  #
+  yabai -m rule --add app="^(Notion|Photoshop|Lightroom|Pym|Slack|Discord|Logic Pro|Home|Controller)$" space=3
   yabai -m rule --add label="^(TauriApp|tauri-app|launchpad|LaunchPad)$" app="^(iTerm2|Min|TauriApp|tauri-app|launchpad|LaunchPad)$" space=^6
   yabai -m rule --add app="^(OmniFocus|Mail|eM Client|Calendar)$" space=5
   yabai -m rule --add app="^Code$" space=^7
