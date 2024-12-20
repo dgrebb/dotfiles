cfb9e3aa1c8c65ae91e9591ebf03f2acf7249dda
Dan Grebb <git@dgrebb.com>
2024-12-20 04:49:42 -0500

    chore(dotfiles): update paths

--- steps/dotfiles.sh
+++ steps/dotfiles.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-DOT=~/Projects/dotfiles/.dotfiles
 
 # Link them
 ln -sf ~/Projects/dotfiles/.dotfiles/.aliases ~/.aliases
 ln -sf ~/Projects/dotfiles/.dotfiles/.functions ~/.functions
-ln -sf ~/Projects/dotfiles/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/Projects/dotfiles/.dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -sf ~/Projects/dotfiles/.dotfiles/.zshrc ~/.zshrc
+
+# Copy, don't link, the .gitconfig file
+rm -rf ~/.gitconfig && cp ~/Projects/dotfiles/.dotfiles/.gitconfig ~/.gitconfig
 
 # Others while we're at it
 ln -sf ~/Projects/dotfiles/.config/iTerm2 ~/.config/iTerm2
@@ -16,6 +17,11 @@ ln -sf ~/Projects/dotfiles/.vscode/settings.json ~/Library/Application\ Support/
 ln -sf ~/Projects/dotfiles/.config/sketchybar ~/.config/sketchybar
 ln -sf ~/Projects/dotfiles/.config/yabai ~/.config/yabai
 ln -sf ~/Projects/dotfiles/.config/machine.sh ~/.config/machine.sh
+# ln -s ~/Projects/dotfiles/.dotfiles/ ~/
+# ln -s ~/Projects/dotfiles/.dotfiles/ ~/
+# ln -s ~/Projects/dotfiles/.dotfiles/ ~/
+# ln -s ~/Projects/dotfiles/.dotfiles/ ~/
+# ln -s ~/Projects/dotfiles/.dotfiles/ ~/
 
 mkdir ~/.sketchyrw
 
