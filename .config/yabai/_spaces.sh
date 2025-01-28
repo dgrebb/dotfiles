bf700232aa36f0184935e52108b22044291a0a57
Dan Grebb <git@dgrebb.com>
2025-01-27 19:53:44 -0500

    chore: yabai space change and vscode settings

--- .config/yabai/_spaces.sh
+++ .config/yabai/_spaces.sh
@@ -120,16 +120,13 @@ if [[ "$MACHINE" == 'home' ]]; then
 
   yabai -m rule --add app="^Music$" space=1
   yabai -m rule --add app="^(Firefox|Zen Browser|Google Chrome|Safari)$" space=^2
-  # Add Home/Controller when done setting up
-  #
-  yabai -m rule --add app="^(Notion|Photoshop|Lightroom|Adobe Lightroom Classic|Pym|Slack|Discord|Logic Pro|Reason 13|Reason Companion|Home|Controller|Zoom|zoom.us|ChatGPT)$" space=^3
-  yabai -m rule --add label="^(TauriApp|tauri-app|launchpad|LaunchPad)$" space=^6
-  yabai -m rule --add app="^(iTerm2|Ghostty|Min|TauriApp|tauri-app|launchpad|LaunchPad)$" space=^6
+  yabai -m rule --add app="^(Notion|Photoshop|Lightroom|Adobe Lightroom Classic|Pym|Slack|Discord|Logic Pro|Reason 13|Reason Companion|Home|Controller|Zoom|zoom.us|ChatGPT)$" space=3
+  yabai -m rule --add app="^(Mail|Canary Mail|eM Client|Calendar)$" space=4
   yabai -m rule --add app="^(OmniFocus)$" space=5
-  yabai -m rule --add app="^(Mail|Canary Mail|eM Client|Calendar)$" space=^4
-  yabai -m rule --add app="^(Visual Studio Code|Cursor)$" space=^7
+  yabai -m rule --add app="^(iTerm2|Ghostty|Min|TauriApp|tauri-app|launchpad|LaunchPad)$" space=6
+  yabai -m rule --add app="^(Code|Cursor)$" space=7
+  yabai -m rule --add app="^dg project$" space=8
   yabai -m rule --add app="^Obsidian$" space=^9
-  yabai -m rule --add app="^dg project$" space=^8
 
 else
 
