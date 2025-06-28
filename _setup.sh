f13e1c304b8cc5e5a5de7730bd2eeaf1da047337
Dan Grebb <445891+dgrebb@users.noreply.github.com>
2025-06-28 03:59:09 -0400

    feat(setup): include Starship in setup script; check `.zshrc` and update accordingly

--- _setup.sh
+++ _setup.sh
@@ -6,8 +6,18 @@ yellow="33"
 BOLDRED="\e[1;${red}m"
 BOLDGREEN="\e[1;${green}m"
 BOLDYELLOW="\e[1;${yellow}m"
-
 NC="\033[0m" # No Color
+
+# Install Starship
+curl -sS https://starship.rs/install.sh | sh
+
+# Add starship init to .zshrc if it doesn't already exist
+if ! grep -q "starship init zsh" ~/.zshrc; then
+  echo 'eval "$(starship init zsh)"' >>~/.zshrc
+  echo "Added starship init to ~/.zshrc"
+else
+  echo "Starship init already exists in ~/.zshrc"
+fi
 
 # Set permissions on shell scripts
 echo "Setting permissions on shell scripts"
