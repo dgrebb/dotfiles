092200b0511095b82728be69061f8cb353e4aaba
Dan Grebb <445891+dgrebb@users.noreply.github.com>
2025-12-13 18:13:37 -0500

    chore: yabai fixes for 1Password helper

--- yabai/.config/yabai/_app-management.sh
+++ yabai/.config/yabai/_app-management.sh
@@ -8,8 +8,10 @@ yabai -m rule --add app="^(Digital Colou?r Meter|Classic Color Meter)$" sticky=o
 yabai -m rule --add app="^(Glucose Graph|Verve|Contrast|Gifox|Bartender 5|Airfoil|Messages|Keyboard Maestro|TextExpander|Calculator|Software Update|Dictionary|VLC|System Preferences|System Settings|Photo Booth|Archive Utility|Python|LibreOffice|App Store|Steam|Alfred|Activity Monitor|sketchybar|OmniPlan)$" manage=off opacity=1.0
 yabai -m rule --add title="(Co(py|nnect)|Move|Info|Pref)" manage=off
 yabai -m rule --add label="Safari" app="^Safari$" title="^(General|(Tab|Password|Website|Extension)s|AutoFill|Se(arch|curity)|Privacy|Advance)$" manage=off
+yabai -m rule --add label="1Password" app="^1Password$" title="^(Settings|Helper|Search)$" manage=off
 yabai -m rule --add label="About This Mac" app="System Information" title="About This Mac" manage=off
 yabai -m rule --add label="Select file to save to" app="^Inkscape$" title="Select file to save to" manage=off
 yabai -m rule --add label="About This Mac" app="System Information" title="About This Mac" manage=off
-yabai -m rule --add app="^(ProtonVPN|GeekTool|CopyClip 2|xScope|iStat Menus Status|com.bjango.istatmenus.weather|Security/Agent)$" manage=off sub-layer=above
-yabai -m rule --add label="1Password" app="^1Password$" title="^(password|Password|Helper)$" manage=off
+yabai -m rule --add app="^(ProtonVPN|GeekTool|CopyClip 2|xScope|iStat Menus Status|com.bjango.istatmenus.weather|Security/Agent)$" sub-layer=above
+
+yabai -m rule --apply
