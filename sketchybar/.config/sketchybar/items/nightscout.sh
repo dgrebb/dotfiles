583bcc07a593ef077cbff1892cdda4037d73d967
Dan Grebb <445891+dgrebb@users.noreply.github.com>
2025-12-24 04:10:39 -0500

    refactor(sketchybar): use nightscout api

--- sketchybar/.config/sketchybar/items/nightscout.sh
+++ sketchybar/.config/sketchybar/items/nightscout.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-ICON=
+LABEL=
 
 nightscout=(
-  drawing=on
-  update_freq=60
-  click_script="open https://glu.7ub3s.net/"
-  background.padding_left=0
-  background.padding_right=0
-  label.padding_right=0
-  background.drawing=off
-  label.padding_right=0
-  icon.padding_right=0
-  icon.padding_left=0
-  label.padding_left=0
   associated_display=1
+  background.padding_left=9
+  background.padding_right=0
+  background.drawing=off
+  click_script="open https://glu.7ub3s.net/"
+  drawing=on
+  icon=0
+  icon.font="$FONT:Black:14.0"
+  icon.padding_right=7
+  icon.y_offset=-1
+  label.y_offset=1
+  label=$LABEL
+  label.font="Hack Nerd Font:Regular:12.0"
+  label.color=$DARK_RED
+  label.padding_right=0
+  label.padding_left=0
+  script="$PLUGIN_DIR/nightscout.sh"
+  update_freq=60
 )
 
-sketchybar --add alias "Glucose Graph,Item-0" right \
-  --set "Glucose Graph,Item-0" "${nightscout[@]}" \
-  click_script="open https://glu.7ub3s.net/" \
-  alias.color=0xffff9341 \
-  y_offset=-1
+# Tahoe/Sketchybar don't support unnamed menubar items
+# sketchybar --add alias "Glucose Graph,Item-0" right \
+#   --set "Glucose Graph,Item-0" "${nightscout[@]}" \
+#   click_script="open https://glu.7ub3s.net/" \
+#   alias.color=0xffff9341 \
+#   y_offset=-1
+
+sketchybar --add item nightscout right \
+  --set nightscout "${nightscout[@]}"
--- sketchybar/.config/sketchybar/plugins/nightscout.sh
+++ sketchybar/.config/sketchybar/plugins/nightscout.sh
@@ -3,25 +3,28 @@
 source "$HOME/.config/sketchybar/colors.sh"
 
 ICON_COLOR=$WHITE
+GLU_API_KEY=$(pass dg/glu/api-key)
 
 data=$(
   curl -X 'GET' \
-    'https://glu.7ub3s.net/api/v1/entries/sgv?count=1' \
+    "https://glu.7ub3s.net/api/v1/entries/sgv?count=1&token=${GLU_API_KEY}" \
     -H 'accept: application/json'
 )
 
 BG=$(echo $data | jq -r '.[0].sgv')
 
-if [[ $BG -gt "233" ]]; then
+if [[ $BG -gt "120" ]]; then
   ICON_COLOR=$YELLOW
-elif [[ $BG -gt "233" ]]; then
+elif [[ $BG -gt "160" ]]; then
   ICON_COLOR=$RED
 elif [[ $BG -gt "300" ]]; then
   ICON_COLOR=$BLACK
-elif [[ $BG -lt "80" ]]; then
+elif [[ $BG -lt "100" ]]; then
   ICON_COLOR=$YELLOW
-elif [[ $BG -lt "75" ]]; then
-  ICON_COLOR=$BLACK
+elif [[ $BG -lt "90" ]]; then
+  ICON_COLOR=$RED
+else
+  ICON_COLOR=$GREEN
 fi
 
 sketchybar --set nightscout icon=$BG icon.color=$ICON_COLOR
